f7d5bfbaf7026142537d3c4d7d7e84abe152975d
AlvaroAbad <alvaro.abadm@gmail.com>
2016-11-11 10:59:12 +0100

    Implemented Smart Pointerss and started Array implementation

---
+++ include/Array.h
@@ -0,0 +1,49 @@
+#ifndef AAGINE_ARRAY_H
+#define AAGINE_ARRAY_H
+
+#include "Essentials.h"
+
+template <typename T>
+class Array {
+public:
+	Array();
+	Array(uint32 size);
+	Array(Array<T>& other);
+	~Array() { delete[] buffer };
+
+	Array<T>& operator=(const Array<T>& other);
+	
+
+	//Adding elements
+	uint32 Size(){return m_numElements}
+	uint32 Push(const T& element);
+	void Set(uint32 index, T& element) { m_buffer[index] = element; }
+	bool Insert(uint32 index, T& element);
+
+	//Removing Elements
+	T PopLast();
+	T PopFirst();
+	bool Remove(T element);
+	bool RemoveAt(uint32 index);
+	uint32 RemoveAll(T element);
+
+	//Accessing Elements
+	T& operator[](uint32 pos) { return m_buffer[pos]; }
+	const T& operator[](uint32 pos) const { return m_buffer[pos]; }
+	T& first() { return m_buffer; }
+	const T& first()const { return m_buffer; }
+	T& Last() { return m_buffer[m_numElements - 1]; }
+	const T& Last() const { return m_buffer[m_numElements - 1]; }
+	T& Next() { return m_buffer[m_nextElement++]; m_nextElement == m_size ? m_nextElement = 0; }
+	const T& Next() const { return m_buffer[m_nextElement++]; m_nextElement == m_size ? m_nextElement = 0; }
+
+	void Sort(bool(*compareFunct)(T&, T&));
+	void Clear();
+private:
+	T* m_buffer;
+	uint32 m_size;
+	uint32 m_numElements;
+	uint32 m_nextElement;
+};
+
+#endif // AAGINE_ARRAY_H
--- include/SmartPTR.h
+++ include/SmartPTR.h
@@ -1,1 +1,99 @@
-#pragma once
+#ifndef AAGINE_SMARTPTR_H
+#define AAGINE_SMARTPTR_H
+template<typename T> class WPTR;
+class PTRReferences {
+	PTRReferences():m_counter(0) {}
+	void Inc() { m_counter++; }
+	unsigned long Dec() { return --m_counter; }
+private:
+	unsigned long m_counter;
+};
+template<typename T>
+class PTR {
+	PTR(): m_Data(nullptr), m_Counter(nullptr) { m_Counter = new PTRReferences(); m_Counter->Inc(); }
+	PTR(T* p): m_Data(p), m_Counter(nullptr) { m_Counter = new PTRReferences(); m_Counter->Inc(); }
+	PTR(T* p, RC* r): m_Data(p), m_Counter(r) { m_Counter->Inc(); }
+	PTR(const PTR<T>& p): m_Data(p.m_Data), m_Counter(p.m_Counter) { m_Counter->Inc(); }
+	PTR(const WPTR<T>& p);
+	~PTR() { if (m_Counter->Dec() == 0) { delete m_Data; delete m_Counter; } }
+	T& operator*() const { return *m_Data; }
+	T* operator->() const { return m_Data; }
+	PTR<T>& operator=(const PTR<T>& p) { if (*this != p) { if (m_Counter->Dec() == 0) { delete m_Data; delete m_Counter; } m_Data = p.m_Data; m_Counter = p.m_Counter; m_Counter->Inc(); } return *this; }
+	bool operator==(const PTR<T>& p) const { return this->m_Data == p.m_Data; }
+	bool operator!=(const PTR<T>& p) const { return this->m_Data != p.m_Data; }
+	bool operator==(const T* const p) const { return this->m_Data == p; }
+	bool operator!=(const T* const p) const { return this->m_Data != p; }
+
+	template <typename Y> PTR<Y> UpCast();
+	template <typename Y> PTR<Y> DownCast();
+	PTR<const T> ConstCast() const { return PTR<const T>(m_Data, m_Counter); }
+
+private:
+	T* m_Data;
+	PTRReferences* m_Counter;
+
+	friend class WPTR<T>;
+};
+
+template <typename T>
+template <typename Y>
+PTR<Y> PTR<T>::UpCast() {
+	if (static_cast<Y*>(m_Data))
+		return PTR<Y>(m_Data, m_Counter);
+	else
+		return PTR<Y>();
+}
+
+template <typename T>
+template <typename Y>
+PTR<Y> PTR<T>::DownCast() {
+	if (dynamic_cast<Y*>(m_Data))
+		return PTR<Y>((Y*)m_Data, m_Counter);
+	else
+		return PTR<Y>();
+}
+
+template<typename T>
+class WPTR {
+public:
+	WPTR(): m_Data(nullptr), m_Counter(nullptr) {}
+	WPTR(T* p): m_Data(p), m_Counter(nullptr) {}
+	WPTR(T* p, PTRReferences* r): m_Data(p), m_Counter(r) {}
+	WPTR(const WPTR<T>& p): m_Data(p.m_Data), m_Counter(p.m_Counter) {}
+	WPTR(const PTR<T>& p): m_Data(p.m_Data), m_Counter(p.m_Counter) {}
+	T& operator*() const { return *m_Data; }
+	T* operator->() const { return m_Data; }
+	WPTR<T>& operator=(const WPTR<T>& p) { m_Data = p.m_Data; m_Counter = p.m_Counter; return *this; }
+	bool operator==(const WPTR<T>& p) const { return this->m_Data == p.m_Data; }
+	bool operator!=(const WPTR<T>& p) const { return this->m_Data != p.m_Data; }
+	bool operator==(const T* const p) const { return this->m_Data == p; }
+	bool operator!=(const T* const p) const { return this->m_Data != p; }
+
+	template <typename Y> WPTR<Y> UpCast();
+	template <typename Y> WPTR<Y> DownCast();
+	WPTR<const T> ConstCast() const { return WPTR<const T>(m_Data, m_Counter); }
+private:
+	T* m_Data;
+	PTRReferences* m_Counter;
+
+	friend class PTR<T>;
+};
+
+template <typename T>
+template <typename Y>
+WPTR<Y> WPTR<T>::UpCast() {
+	if (static_cast<Y*>(m_Data))
+		return WPTR<Y>(m_Data, m_Counter);
+	else
+		return WPTR<Y>();
+}
+
+template <typename T>
+template <typename Y>
+WPTR<Y> WPTR<T>::DownCast() {
+	if (dynamic_cast<Y*>(m_Data))
+		return WPTR<Y>((Y*)m_Data, m_Counter);
+	else
+		return WPTR<Y>();
+}
+#endif // !AAGINE_SMARTPTR_H
